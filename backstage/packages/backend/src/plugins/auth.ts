0879538049961ac993d888680f570e63f32e71b3
Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>
2024-04-03 13:01:22 -0400

    do the user auth better using built ins
    Signed-off-by: Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>

--- backstage/packages/backend/src/plugins/auth.ts
+++ backstage/packages/backend/src/plugins/auth.ts
@@ -5,7 +5,6 @@ import {
 } from '@backstage/plugin-auth-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import { DEFAULT_NAMESPACE, stringifyEntityRef, } from '@backstage/catalog-model';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -52,28 +51,9 @@ export default async function createPlugin(
       }),
       google: providers.google.create({
         signIn: {
-          resolver: async ({ profile }, ctx) => {
-            if (!profile.email) {
-              throw new Error(
-                'Login failed, user profile does not contain an email',
-              );
-            }
-            const [localPart] = profile.email.split('@');
-            const userEntityRef = stringifyEntityRef({
-              kind: 'User',
-              name: localPart,
-              namespace: DEFAULT_NAMESPACE,
-            });
-
-            return ctx.issueToken({
-              claims: {
-                sub: userEntityRef,
-                ent: [userEntityRef],
-              },
-            });
-          },
+          resolver: providers.google.resolvers.emailLocalPartMatchingUserEntityName(),
         },
-      }),
+      });
     },
   });
 }
